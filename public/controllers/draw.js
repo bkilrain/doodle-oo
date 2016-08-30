8700f1c994ccb7f0e3921fdedc2389d325baf0bf
Brian Kilrain <bkilrain@gmail.com>
2016-08-30 15:27:24 -0700

    Add brush width functionality

--- public/controllers/draw.js
+++ public/controllers/draw.js
@@ -7,10 +7,12 @@ angular.module('doodle.draw', [
   $scope.color = "#005E7A"
   $scope.canvas.freeDrawingBrush.color = $scope.color;
   $scope.url;
+  $scope.width = 4;
+  $scope.canvas.freeDrawingBrush.width = $scope.width;
   $scope.saveCanvas = function() {
     Canvases.addCanvas(JSON.stringify($scope.canvas.toJSON()))
       .then(function(data) {
-        $scope.url = 'http://localhost:3000/#/saved/' + data.data;
+        $scope.url = 'http://45.55.27.86:3000/#/saved/' + data.data;
         console.log($scope.url)
       });
   };
@@ -21,6 +23,8 @@ angular.module('doodle.draw', [
   $scope.changeColor = function() {
     $scope.canvas.freeDrawingBrush.color = $scope.color;
   }
-
+  $scope.changeWidth = function() {
+    $scope.canvas.freeDrawingBrush.width = $scope.width;
+  }
   
 })
